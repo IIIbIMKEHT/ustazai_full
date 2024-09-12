6bea2826da0935f3625292273f1ac75071472ba2
IIIbIMKEHT <nurbakit_5496@mail.ru>
2024-09-12 16:15:31 +0500

    fixed bugs

--- laravel/resources/views/livewire/generate-material.blade.php
+++ laravel/resources/views/livewire/generate-material.blade.php
@@ -149,8 +149,9 @@
                             </label>
                         @endif
                         <label class="block">
-
                             <input
+                                wire:loading.attr="disabled"
+                                wire:target="send"
                                 class="form-input mt-1.5 w-full rounded-lg border border-slate-300 bg-transparent px-3 py-2 placeholder:text-slate-400/70 hover:border-slate-400 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent"
                                 type="submit"
                                 value="Отправить"
