d71fb1b08870cb776632e2728d5cffdd5c5e12fb
nurba <kazitech2023@gmail.com>
2024-09-15 20:18:06 +0500

    auth fix

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -21,10 +21,9 @@ class AuthController extends Controller
         ]);
         
         if (Auth::attempt($credentials)) {
-            
-            $request->session()->regenerate();
- 
             return redirect(route('dashboard'));
+        } else {
+            abort(404);
         }
     }
     public function google()
